5d8bd921e2737afbf9b5d463241143fc267f80ea
Raghav Sood <raghav1sood@yahoo.co.in>
2026-03-31 17:15:04 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,18 @@
+from functools import cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        @cache
+        def dp(i):
+
+            if i >= len(nums):return 0
+
+            return max(nums[i]+dp(i+2), dp(i+1))
+
+
+
+        
+        return max(dp(0), dp(1))
+
+        
