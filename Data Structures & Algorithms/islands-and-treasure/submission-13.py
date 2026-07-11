28218d2eec9f166cb5523ee5d2ee859c25cf5f46
Raghav Sood <raghav1sood@yahoo.co.in>
2026-07-11 14:58:33 -0700

    Add: islands-and-treasure - submission-13

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-13.py
@@ -0,0 +1,42 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        q = []
+        
+
+        def bfs(q):
+            step = 1
+
+            
+            while q:
+                print("here")
+                tempq = []
+                for m,n in q:
+                    for d in directions:
+                        x,y = m+d[0], n + d[1]
+
+                        if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y] == 2147483647:
+                            grid[x][y] = step
+                            tempq.append((x,y))
+                            
+
+                q = tempq
+                step+=1
+
+            
+
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+
+                if grid[row][col] == 0:
+                    q.append((row,col))
+                    
+        bfs(q)
+
+
+                
+                
+
